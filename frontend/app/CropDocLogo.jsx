4a5e857fae504d8beafab6cb4b1c3cbb362cfbf2
Bart-Desktop <Bartjstolarek@gmail.com>
2024-08-15 20:33:02 +1000

    completed landing page basics

---
+++ frontend/app/CropDocLogo.jsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React from 'react';
+import { useTheme } from "next-themes"; // Import from next-themes
+
+const CropDocLogo = () => {
+  const { theme } = useTheme();
+  const isDark = theme === 'dark'; // Check if the theme is dark
+
+  return (
+    <div className="flex items-center">
+      <span 
+        className={`text-4xl font-bold -top-3 ${isDark ? 'text-gray-100' : 'text-gray-900'} relative`}
+      >
+        Crop
+        <span 
+          className={`absolute -bottom-4 left-0 text-4xl ${isDark ? 'text-green-300' : 'text-green-700'}`}
+        >
+          Doc
+        </span>
+      </span>
+    </div>
+  );
+};
+
+export default CropDocLogo;
