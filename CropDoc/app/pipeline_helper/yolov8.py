e57c916b57118b2f16298e06e3778459725e4f3e
Benjamin Nolan <benjamin.nolan@slalom.com>
2024-08-27 13:50:04 +1000

    updated yolov8 config

--- CropDoc/app/pipeline_helper/yolov8.py
+++ CropDoc/app/pipeline_helper/yolov8.py
@@ -3,7 +3,7 @@
 # model.train(data='classify', epochs=10, imgsz=64, batch=512)
 
 import sys
-from dataset import CropCCMTDataset
+# from dataset import CropCCMTDataset
 
 from ultralytics import YOLO, checks, hub
 checks()
@@ -12,11 +12,20 @@ hub.login('fc32f0556a2215712769d62a84dcc06cbb5635077b')
 
 dataset_dir = '../../../../yolov5/datasets/classify'
 
+model = {
+    'hub': 'https://hub.ultralytics.com/models/uu2o2jCUUozCt7grEZ7l',
+    'local': 'runs/classify/train22/weights/best.pt',
+}
 
-# y_model = 'https://hub.ultralytics.com/models/uu2o2jCUUozCt7grEZ7l'
-# y_model = 'yolov8n-cls.pt'
-y_model = 'runs/classify/train22/weights/best.pt'
-y_config = 'yolov8.yaml'
+config = {
+    'custom': 'yolov8.yaml'
+}
+# y_model = 'https://hub.ultralytics.com/models/uu2o2jCUUozCt7grEZ7l' # last hub model trained
+# y_model = 'yolov8n-cls.pt'  # pre-trained yolov8 model
+# y_model = 'runs/classify/train22/weights/best.pt'  # last local trained model based on yolov8 pretrained model
+# y_config = 'yolov8.yaml'  # yolov8 config
+
+y_model = model['local']
 
 def train():
     model = YOLO(y_model)
